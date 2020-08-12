6396dbd5ee1cbd336e98c66e227aabebd61aabc1
Andrew Monshizadeh <amonshiz@gmail.com>
2020-08-12 10:16:36 -0500

    Added project

---
+++ appstore-swift/Shared/appstore_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  appstore_swiftApp.swift
+//  Shared
+//
+//  Created by Andrew Monshizadeh on 8/12/20.
+//
+
+import SwiftUI
+
+@main
+struct appstore_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
